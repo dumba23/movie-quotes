a07c665ecdf413051a3009e01a2b942703c11a42
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-05-15 20:43:53 +0400

    feat: add assets and custom colors for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-yellow": "#DDCCAA",
+        "primary-red": "#E31221",
+        "primary-grey": "#D9D9D9",
+      },
+      backgroundImage: {
+        "primary-home":
+          "linear-gradient(180deg, #11101A 0%, #08080D 65.52%, rgba(0, 0, 0, 0) 100%)",
+        interstellar:
+          "linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 55.21%, rgba(0, 0, 0, 0) 100%)",
+        inter: "url('@/assets/images/interstellar.png')",
+      },
+    },
   },
   plugins: [],
 };
