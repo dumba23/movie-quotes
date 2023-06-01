d7ff76cdb97164452458202c98b82eef37df110a
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-01 16:35:15 +0400

    refactor(env): extract hardcoded string into env

--- src/plugins/axios/axios.js
+++ src/plugins/axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const defaultInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
