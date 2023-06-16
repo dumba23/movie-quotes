480d79743e019e93ef69f0babf16ca7c2eeb2c3f
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-16 19:02:06 +0400

    feat(tailwind): add custom colors into config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,22 @@ export default {
         "primary-success": "#198754",
         "primary-blue": "#0D6EFD",
         "secondary-grey": "#6C757D",
+        gray: "#9C9A9A",
         "grey-dim": "#222030",
         "secondary-red": "#DC3545",
         "light-grey": "#CED4DA",
         "light-red": "#E31221",
+        "primary-black": "#11101A",
       },
       backgroundImage: {
         "primary-home":
           "linear-gradient(180deg, #11101A 0%, #08080D 100%, rgba(0, 0, 0, 0) 100%)",
+        "primary-profile":
+          "linear-gradient(187.16deg, #181623 0.07%, #191725 51.65%, #0D0B14 98.75%)",
         interstellar:
           "linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 55.21%, rgba(0, 0, 0, 0) 100%)",
+        header:
+          "linear-gradient(0deg, rgba(34, 32, 48, 0.2), rgba(34, 32, 48, 0.2))",
       },
       boxShadow: {
         input: "0px 0px 0px 4px rgba(13, 110, 253, 0.25)",
