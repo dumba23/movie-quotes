806d38eced0bb1ebe0de3411a7cc33a7ac65e125
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-16 19:04:33 +0400

    feat(service): add methods for api endpoint fetch

---
+++ src/services/user.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import axiosInstance from "@/plugins/axios/axios.js";
+
+export const getUserData = async () => {
+  await axiosInstance.get("/sanctum/csrf-cookie");
+  return await axiosInstance.get("/api/user");
+};
+export const editUserData = async (data) => {
+  await axiosInstance.get("/sanctum/csrf-cookie");
+  return await axios.post(
+    import.meta.env.VITE_API_BASE_URL + "/api/user/edit",
+    data,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+};
