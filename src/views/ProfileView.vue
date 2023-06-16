f4e5624a49769bea5a06919301826f690f763427
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-16 19:06:55 +0400

    feat(profile): add profile page

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,35 @@
+<template>
+  <TheLoggedInHeader />
+  <div class="flex text-xl">
+    <div class="max-w-1/3">
+      <ProfileSidebar
+        :username="userStore.user.username"
+        :imageUrl="userStore.user.avatar"
+      />
+    </div>
+    <div class="min-w-full absolute flex items-center justify-start flex-col">
+      <div class="space-y-12 mt-6 text-white">
+        <h2 class="font-medium">My profile</h2>
+        <div
+          class="bg-primary-black px-40 py-20 pt-40 relative rounded-xl translate-y-20"
+        >
+          <ProfileEditForm />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TheLoggedInHeader from "@/components/shared/TheLoggedInHeader.vue";
+import ProfileSidebar from "@/components/ProfileSidebar.vue";
+import ProfileEditForm from "@/components/ProfileEditForm.vue";
+import { useUserStore } from "../store/user";
+import { onMounted } from "vue";
+
+const userStore = useUserStore();
+
+onMounted(() => {
+  userStore.initializeUserData();
+});
+</script>
