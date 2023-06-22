9d384db90196fcb180ac966f5a7fa37c6d198865
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-22 19:42:08 +0400

    feat(form): create add and edit forms for movie

---
+++ src/components/MovieEdit.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="p-10">
+    <div class="flex items-center mb-8">
+      <img
+        :src="
+          profileImageUrl.includes('http')
+            ? profileImageUrl
+            : apiUrl + '/storage/' + profileImageUrl
+        "
+        alt="profile"
+        class="w-14 h-14 rounded-full mr-3"
+      />
+      <div>{{ username }}</div>
+    </div>
+    <form @submit.prevent="submitForm">
+      <MovieInput
+        type="text"
+        labelName="Movie name:"
+        name="title_en"
+        :errorMessage="errorMessage"
+        rules="required"
+      />
+      <MovieInput
+        type="text"
+        labelName="ფილმის სახელი:"
+        name="title_ka"
+        :errorMessage="errorMessage"
+        rules="required"
+      />
+      <GenresInput :genres="genres" :initialValue="movie.genres" />
+      <MovieInput
+        type="text"
+        labelName="წელი/Year:"
+        name="release_date"
+        :errorMessage="errorMessage"
+        rules="required"
+      />
+      <MovieInput
+        type="text"
+        labelName="Director:"
+        name="director_en"
+        :errorMessage="errorMessage"
+        rules="required"
+      />
+      <MovieInput
+        type="text"
+        labelName="რეჟისორი:"
+        name="director_ka"
+        :errorMessage="errorMessage"
+        rules="required"
+      />
+      <MovieTextarea
+        rules="required"
+        labelName="Description:"
+        name="description_en"
+      />
+      <MovieTextarea
+        rules="required"
+        labelName="აღწერა:"
+        name="description_ka"
+      />
+      <MovieFileUpload :initialValue="movie.image" />
+      <MovieSubmit name="Update movie" />
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { useForm } from "vee-validate";
+import MovieInput from "@/components/ui/MovieInput.vue";
+import MovieFileUpload from "@/components/ui/MovieFileUpload.vue";
+import MovieTextarea from "@/components/ui/MovieTextarea.vue";
+import MovieSubmit from "@/components/ui/MovieSubmit.vue";
+import GenresInput from "@/components/ui/GenresInput.vue";
+import { addMovie } from "@/services/movies";
+import { onBeforeMount, ref } from "vue";
+import { getMovieGenres } from "@/services/movies";
+import { useRoute, useRouter } from "vue-router";
+
+const props = defineProps({
+  username: { type: String, required: true, default: "" },
+  profileImageUrl: { type: String, required: true, default: "" },
+  movie: { type: Object, required: true, default: () => {} },
+});
+
+const route = useRoute();
+const router = useRouter();
+
+const formValues = {
+  title_en: props.movie.title.en,
+  title_ka: props.movie.title.ka,
+  director_en: props.movie.director.en,
+  director_ka: props.movie.director.ka,
+  description_en: props.movie.description.en,
+  description_ka: props.movie.description.ka,
+  release_date: props.movie.release_date,
+};
+
+const { values } = useForm({ initialValues: formValues });
+
+const errorMessage = ref("");
+const genres = ref([]);
+
+const submitForm = async () => {
+  const {
+    params: { id },
+  } = route;
+  const data = values;
+  if (data.image == undefined) {
+    delete data.image;
+  }
+
+  try {
+    const res = await addMovie(data, id);
+    if (res.status === 200) {
+      router.push({ name: "movies" });
+    }
+  } catch (error) {
+    const { status, message } = error.response.data;
+    if (!status) {
+      errorMessage.value = message;
+    }
+  }
+};
+
+onBeforeMount(async () => {
+  try {
+    const res = await getMovieGenres();
+    if (res.status === 200) {
+      genres.value = res.data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+</script>
