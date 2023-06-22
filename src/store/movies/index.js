ca9067ad390f84c6b50989d1c69e1c21d3116143
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-22 19:32:28 +0400

    feat(store): create movie store

---
+++ src/store/movies/index.js
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia";
+import { getUserMoviesList, getUserMovie } from "@/services/movies";
+
+export const useMoviesStore = defineStore("movies", {
+  state: () => ({
+    movies: [],
+    movie: {},
+  }),
+  actions: {
+    async initializeMoviesData() {
+      try {
+        const res = await getUserMoviesList();
+        if (res.status === 200) {
+          this.movies = [...res.data];
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async initializeMovieData(payload) {
+      try {
+        const res = await getUserMovie(payload.id);
+        if (res.status === 200) this.movie = res.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    clearMovieData() {
+      this.movie = {};
+    },
+  },
+});
