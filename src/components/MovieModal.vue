13de4cf71093e0c09a4da768cae563e21379f5cd
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-22 19:45:34 +0400

    feat: add modal for movie forms

---
+++ src/components/MovieModal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="fixed inset-0 flex items-center z-10">
+    <div
+      class="absolute bg-primary-black bg-opacity-30 left-0 right-0 top-0 bottom-0"
+    >
+      <div
+        class="relative mx-auto w-[1000px] shadow-lg rounded-md bg-primary-black flex flex-col justify-center translate-y-32"
+        id="modal"
+      >
+        <slot name="header"></slot>
+        <slot name="form"></slot>
+      </div>
+    </div>
+  </div>
+</template>
