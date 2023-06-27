0c959cc93f1c2272cabe8d2fd9141aa6de1bc935
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-06-27 12:44:35 +0400

    feat(service): add api methods

---
+++ src/services/quotes.js
@@ -0,0 +1,43 @@
+import axios from "@/plugins/axios/axios.js";
+
+export const getUserQuotesList = async () => {
+  await axios.get("/sanctum/csrf-cookie");
+  return await axios.get("/api/quotes");
+};
+
+export const getUserQuote = async (quoteId) => {
+  await axios.get("/sanctum/csrf-cookie");
+  return await axios.get("/api/quotes/" + quoteId);
+};
+
+export const addQuote = async (data) => {
+  await axios.get("/sanctum/csrf-cookie");
+  return await axios.post("/api/quotes", data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+export const updateQuote = async (data, quoteId) => {
+  await axios.get("/sanctum/csrf-cookie");
+
+  const formData = new FormData();
+
+  formData.append("_method", "PUT");
+
+  Object.keys(data).forEach((key) => {
+    formData.append(key, data[key]);
+  });
+
+  return await axios.post("/api/quotes/" + quoteId, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+export const deleteQuote = async (quoteId) => {
+  await axios.get("/sanctum/csrf-cookie");
+  return await axios.delete("/api/quotes/" + quoteId);
+};
