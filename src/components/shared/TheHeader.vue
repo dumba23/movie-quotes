b2de51ddb523bf8d64e03c06ba0fbe8ac55e8519
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-07-12 18:49:17 +0400

    refactor: add translations and update design for header components

--- src/components/shared/TheHeader.vue
+++ src/components/shared/TheHeader.vue
@@ -1,25 +1,42 @@
 <template>
-  <div class="flex justify-between mx-20 pt-8">
+  <div class="flex justify-between mx-20 pt-8 sm:mx-4">
     <div class="text-primary-yellow">MOVIE QUOTES</div>
-    <div class="flex justify-between w-80">
-      <div class="flex justify-center items-center text-white px-3 py-2">
-        <div class="mr-2.5">Eng</div>
+    <div class="flex justify-between w-80 sm:w-32">
+      <div
+        class="flex justify-center items-center text-white px-3 py-2 sm:hidden cursor-pointer"
+        @click="toggleLanguageDropdown"
+      >
+        <div class="mr-2.5 relative">
+          {{ i18n.global.locale.value === "en" ? "Eng" : "ქარ" }}
+          <div
+            v-if="isLanguageDropdownOpen"
+            ref="languageRef"
+            class="absolute bg-primary-black py-1 px-4 -translate-x-2 translate-y-2"
+            @click="
+              handleSwitchlanguage(
+                i18n.global.locale.value === 'en' ? 'ka' : 'en'
+              )
+            "
+          >
+            {{ i18n.global.locale.value === "en" ? "ქარ" : "Eng" }}
+          </div>
+        </div>
         <div><IconCaretDown /></div>
       </div>
       <div class="flex">
         <button
           @click="handleModalName('register')"
-          class="px-7 py-2 bg-primary-red rounded text-white"
+          class="px-7 py-2 bg-primary-red rounded text-white sm:px-2.5 sm:py-0 sm:text-xs sm:h-8"
         >
-          Sign Up
+          {{ $t("sign_up") }}
         </button>
       </div>
       <div>
         <button
           @click="handleModalName('auth')"
-          class="px-7 py-2 text-white border border-white rounded"
+          class="px-7 py-2 text-white border border-white rounded sm:px-2.5 sm:py-0 sm:text-xs sm:h-8"
         >
-          Log in
+          {{ $t("log_in") }}
         </button>
       </div>
     </div>
@@ -29,8 +46,29 @@
 <script setup>
 import IconCaretDown from "@/components/icons/IconCaretDown.vue";
 import { usePaginationStore } from "@/store/pagination";
+import { switchLanguage } from "@/services/lang";
+import { ref } from "vue";
+import i18n from "@/plugins/i18";
 
 const paginationStore = usePaginationStore();
+const isLanguageDropdownOpen = ref(false);
+const languageRef = ref(null);
+
+const toggleLanguageDropdown = () => {
+  isLanguageDropdownOpen.value = !isLanguageDropdownOpen.value;
+};
+
+const handleSwitchlanguage = async (lang) => {
+  try {
+    const res = await switchLanguage(lang);
+    if (res.status === 200) {
+      i18n.global.locale.value = lang;
+      localStorage.setItem("locale", lang);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const handleModalName = (name) => {
   document.documentElement.style.overflow = "hidden";
--- src/components/shared/TheLoggedInHeader.vue
+++ src/components/shared/TheLoggedInHeader.vue
@@ -1,11 +1,34 @@
 <template>
   <div
-    class="flex justify-between items-center px-20 py-8 bg-header bg-white bg-opacity-5 relative"
+    class="flex justify-between items-center px-20 sm:px-4 py-8 bg-header bg-white bg-opacity-5 relative"
   >
-    <div class="text-primary-yellow">MOVIE QUOTES</div>
-    <div class="flex justify-between items-center w-80">
-      <div class="flex relative">
-        <IconBell @click="handleToggleDropdown" />
+    <div class="text-primary-yellow sm:hidden">MOVIE QUOTES</div>
+    <div class="hidden sm:block"><IconList @click="toggleSidebar" /></div>
+    <transition
+      appear
+      enter-from-class="transform ease-in transition  scale-x-[0] translate-x-[-50%]"
+      enter-active-class="transform ease-in duration-500 transition delay-300"
+    >
+      <div
+        v-if="isSidebarOpen"
+        ref="sidebarRef"
+        class="fixed bg-primary-black left-0 top-0 flex w-3/4 flex-col z-40 bg-gray-700 pt-6 pb-8 h-80"
+      >
+        <ProfileSidebar
+          :username="userStore.user.username"
+          :imageUrl="userStore.user.avatar"
+        />
+        <button
+          @click="handleLogOut"
+          class="px-7 py-2 ml-6 mr-28 text-white border border-white rounded"
+        >
+          {{ $t("log_out") }}
+        </button>
+      </div>
+    </transition>
+    <div class="flex justify-between items-center w-80 sm:justify-end">
+      <div class="flex sm:justify-center relative">
+        <IconBell @click="handleToggleDropdown" class="sm:w-5" />
         <span
           v-if="countUnreadNotifications() > 0"
           class="absolute text-white w-5 h-5 flex justify-center items-center rounded-full z-10 bg-primary-orange right-0 translate-x-[25%] translate-y-[-25%]"
@@ -13,34 +36,55 @@
         >
         <div
           v-if="isDropdownOpen"
-          class="absolute flex flex-col bg-black text-white top-10 w-[36rem] translate-x-[-44%] p-4 py-8 z-20"
+          ref="notificationsRef"
+          class="absolute overflow-y-scroll max-h-96 notifications-dropdown flex flex-col bg-black text-white top-10 w-[36rem] translate-y-16 rounded-xl translate-x-[-44%] p-4 py-8 z-20 sm:w-[23.5rem] sm:translate-x-[-43%] sm:-translate-y-0"
         >
           <div class="flex justify-between items-center mb-5">
-            <div class="font-medium text-2xl">Notifications</div>
+            <div class="font-medium text-2xl tooltiptext">
+              {{ $t("notifications") }}
+            </div>
             <div
               @click="handleMarkAllNotificationRead"
               class="underline cursor-pointer text-lg"
             >
-              Mark all as read
+              {{ $t("mark_all_as_read") }}
             </div>
           </div>
           <QuoteNotificationCard
+            ref="notificationCardRef"
             v-for="notification in notificationsStore.notifications"
             :key="notification.id"
             :notification="notification"
           />
         </div>
       </div>
-      <div class="flex justify-center items-center text-white px-3 py-2">
-        <div class="mr-2.5">Eng</div>
+      <div
+        class="flex justify-center items-center text-white px-3 py-2 sm:hidden cursor-pointer"
+        @click="toggleLanguageDropdown"
+      >
+        <div class="mr-2.5 relative">
+          {{ i18n.global.locale.value === "en" ? "Eng" : "ქარ" }}
+          <div
+            v-if="isLanguageDropdownOpen"
+            ref="languageRef"
+            class="absolute bg-primary-black py-1 px-4 -translate-x-2 translate-y-2"
+            @click="
+              handleSwitchlanguage(
+                i18n.global.locale.value === 'en' ? 'ka' : 'en'
+              )
+            "
+          >
+            {{ i18n.global.locale.value === "en" ? "ქარ" : "Eng" }}
+          </div>
+        </div>
         <div><IconCaretDown /></div>
       </div>
-      <div>
+      <div class="sm:hidden">
         <button
           @click="handleLogOut"
           class="px-7 py-2 text-white border border-white rounded"
         >
-          Log out
+          {{ $t("log_out") }}
         </button>
       </div>
     </div>
@@ -50,18 +94,68 @@
 <script setup>
 import IconBell from "@/components/icons/IconBell.vue";
 import IconCaretDown from "@/components/icons/IconCaretDown.vue";
+import IconList from "@/components/icons/IconList.vue";
 import QuoteNotificationCard from "@/components/QuoteNotificationCard.vue";
+import Cookies from "js-cookie";
 import { useNotificationsStore } from "@/store/notifications";
+import { useUserStore } from "@/store/user";
 import { markAllAsRead } from "@/services/notification";
+import { switchLanguage } from "@/services/lang";
 import { logoutUser } from "@/services/auth";
 import { useRouter } from "vue-router";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
+import ProfileSidebar from "../ProfileSidebar.vue";
+import { onClickOutside } from "@vueuse/core";
+import i18n from "../../plugins/i18";
 
 const router = useRouter();
 const notificationsStore = useNotificationsStore();
+const userStore = useUserStore();
 const isDropdownOpen = ref(false);
+const notificationsRef = ref(null);
+const isSidebarOpen = ref(false);
+const sidebarRef = ref(null);
+const isLanguageDropdownOpen = ref(false);
+const languageRef = ref(null);
+const scrollEventInitialized = ref(false);
+
+const notificationCardRef = ref(null);
+
+const toggleLanguageDropdown = () => {
+  isLanguageDropdownOpen.value = !isLanguageDropdownOpen.value;
+};
+
+const toggleSidebar = () => {
+  isSidebarOpen.value = !isSidebarOpen.value;
+};
+
+const closeSidebar = () => {
+  isSidebarOpen.value = false;
+};
+
+onClickOutside(sidebarRef, closeSidebar);
+
+onClickOutside(notificationsRef, () => {
+  isDropdownOpen.value = false;
+});
+
+onClickOutside(languageRef, () => {
+  isLanguageDropdownOpen.value = false;
+});
+
+const handleScroll = () => {
+  const container = notificationsRef.value;
+  const { scrollTop, clientHeight, scrollHeight } = container;
+  if (scrollTop + clientHeight >= scrollHeight) {
+    notificationsStore.loadMoreNotifications();
+  }
+};
 
 const handleToggleDropdown = () => {
+  if (!isDropdownOpen.value) {
+    notificationsStore.markVisibleAsRead();
+    notificationsStore.initializeNotificationsData();
+  }
   isDropdownOpen.value = !isDropdownOpen.value;
 };
 
@@ -85,6 +179,7 @@ const countUnreadNotifications = () => {
 const handleLogOut = async () => {
   try {
     const res = await logoutUser();
+    Cookies.remove("XSRF-TOKEN");
     if (res.status === 200) {
       router.push({ name: "home" });
     }
@@ -93,7 +188,55 @@ const handleLogOut = async () => {
   }
 };
 
+const handleSwitchlanguage = async (lang) => {
+  try {
+    const res = await switchLanguage(lang);
+    if (res.status === 200) {
+      i18n.global.locale.value = lang;
+      localStorage.setItem("locale", lang);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+watch(
+  notificationsRef,
+  (newRef) => {
+    if (newRef && !scrollEventInitialized.value) {
+      newRef.addEventListener("scroll", handleScroll);
+      scrollEventInitialized.value = true;
+    }
+  },
+  { immediate: true }
+);
+
 onMounted(() => {
   notificationsStore.initializeNotificationsData();
 });
 </script>
+
+<style scoped>
+@media only screen and (min-width: 600px) {
+  .tooltiptext {
+    z-index: 2;
+    color: white;
+    border-radius: 10px;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+  }
+
+  .tooltiptext::before {
+    content: "";
+    position: absolute;
+    border: 10px solid;
+    border-bottom: solid 50px black;
+    border-top: solid 50px transparent;
+    border-left: solid 30px transparent;
+    border-right: solid 30px transparent;
+    transform: translate(146px, -70px);
+    z-index: 1;
+  }
+}
+</style>
