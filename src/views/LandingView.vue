7d5a9a27dd4f92d036555fd7888112450e6874b2
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-07-12 18:49:43 +0400

    refactor: add translations and update design for landing

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -2,17 +2,17 @@
   <TheHeader />
   <div class="flex flex-col justify-center items-center mt-72">
     <div class="text-primary-yellow">
-      <div class="flex flex-col items-center text-6xl mb-6">
-        <span>Find any quote in </span>
-        <span>millions of movie lines</span>
+      <div class="flex flex-col items-center text-6xl mb-6 sm:text-2xl">
+        <span class="sm:font-bold">{{ $t("find_any_quote_in") }} </span>
+        <span class="sm:font-bold">{{ $t("millions_of_movie_lines") }}</span>
       </div>
     </div>
-    <div>
+    <div class="sm:mt-2">
       <button
         class="bg-primary-red px-4 py-2 rounded text-white mb-40"
         @click="handleModalName('register')"
       >
-        Get Started
+        {{ $t("get_started") }}
       </button>
     </div>
     <RegistrationView v-if="paginationStore.modalName == 'register'" />
@@ -30,70 +30,77 @@
     />
     <LinkExpiredModal v-else-if="paginationStore.modalName == 'link_expired'" />
     <SuccessModal v-else-if="paginationStore.modalName == 'success'" />
-    <div id="1" ref="first" @click="handleImageClick(first)" class="relative">
-      <div class="flex absolute top-[341px] left-[170px] items-center">
-        <div class="w-[53px] bg-white h-[2px] mr-8 mb-28"></div>
-        <div class="flex flex-col">
-          <div class="text-5xl text-white w-[700px] mb-8">
-            <p>
-              “You have to leave somethig <br />
-              behind to go forward”
-            </p>
-          </div>
-          <div class="w-[700px] text-3xl text-primary-grey font-bold">
-            Interstellar, 2014
-          </div>
-        </div>
-      </div>
-      <img
-        src="@/assets/images/interstellar.png"
-        alt="interstellar"
-        class="h-full w-screen"
-      />
-    </div>
-    <div id="2" ref="second" @click="handleImageClick(second)">
-      <div class="flex absolute top-[2204px] left-[170px] items-center">
-        <div class="w-[53px] bg-white h-[2px] mr-8 mb-52"></div>
-        <div class="flex flex-col">
-          <div class="w-[900px] mb-6">
-            <p class="text-[50px] text-white font-bold">
-              I think we’re just gonna have <br />
-              to be secretly in love with earch other <br />
-              and leave it that
-            </p>
-          </div>
-          <div class="w-[700px] text-3xl text-primary-grey font-bold">
-            The Royal Tenenbaums,2001
+    <div class="h-full w-full">
+      <div
+        class="min-h-screen min-w-screen sm:max-h-96 sm:bg-cover sm:bg-top sm:min-h-parallax-small justify-center items-center bg-para1 bg-fixed bg-no-repeat bg-cover"
+      >
+        <div
+          class="flex justify-center translate-x-[-10rem] translate-y-[10rem] sm:translate-x-0 sm:translate-y-4 items-center"
+        >
+          <div
+            class="w-[53px] bg-white h-[2px] mr-8 mb-28 sm:mr-2 sm:ml-7 sm:mb-0 sm:translate-y-[-2.3rem]"
+          ></div>
+          <div class="flex flex-col">
+            <div
+              class="text-5xl text-white w-[700px] sm:text-sm sm:max-w-[330px] mb-8"
+            >
+              <p class="font-bold">
+                “{{ $t("you_have_to_leave_something") }} <br />
+                {{ $t("behind_to_go_forward") }}”
+              </p>
+            </div>
+            <div
+              class="w-[700px] sm:max-w-[330px] text-3xl sm:text-sm text-primary-grey font-bold"
+            >
+              {{ $t("interstellar") }}, 2014
+            </div>
           </div>
         </div>
       </div>
-      <img
-        src="@/assets/images/tenenbaums.png"
-        alt="tenenbaums"
-        class="h-full w-screen"
-      />
-    </div>
-    <div id="3" ref="third" @click="handleImageClick(third)">
-      <div class="flex absolute top-[3504px] left-[170px] items-center">
-        <div class="w-[53px] bg-white h-[2px] mr-8 mb-52"></div>
-        <div class="flex flex-col">
-          <div class="w-[900px] mb-6">
-            <p class="text-[50px] text-white font-bold">
-              I think we’re just gonna have <br />
-              to be secretly in love with earch other <br />
-              and leave it that
-            </p>
-          </div>
-          <div class="w-[700px] text-3xl text-primary-grey font-bold">
-            The Royal Tenenbaums,2001
+
+      <div
+        class="min-h-screen min-w-screen flex justify-center sm:bg-contain sm:min-h-max items-center bg-para2 bg-fixed bg-no-repeat bg-cover"
+      >
+        <div class="flex translate-x-[-4rem] items-center">
+          <div class="w-[53px] bg-white h-[2px] mr-8 mb-40"></div>
+          <div class="flex flex-col">
+            <div class="w-[900px] sm:max-w-[330px] mb-6">
+              <p class="text-5xl text-white sm:text-sm font-bold">
+                {{ $t("i_think_we_just_gonna_have") }} <br />
+                {{ $t("to_be_secretly_in_love_with_each_other") }} <br />
+                {{ $t("and_leave_it_that") }}
+              </p>
+            </div>
+            <div
+              class="w-[700px] sm:max-w-[330px] text-3xl sm:text-sm text-primary-grey font-bold"
+            >
+              {{ $t("the_royal_tenenbaums") }},2001
+            </div>
           </div>
         </div>
       </div>
-      <img
-        src="@/assets/images/tenenbaums2.png"
-        alt="tenenbaums"
-        class="h-full w-screen"
-      />
+
+      <div
+        class="min-h-screen min-w-screen flex justify-center sm:bg-contain sm:min-h-max items-center bg-para3 bg-fixed bg-no-repeat bg-cover"
+      >
+        <div class="flex translate-x-[-4rem] items-center">
+          <div class="w-[53px] bg-white h-[2px] mr-8 mb-40"></div>
+          <div class="flex flex-col">
+            <div class="w-[900px] sm:max-w-[330px] mb-6">
+              <p class="text-5xl text-white sm:text-sm font-bold">
+                {{ $t("i_think_we_just_gonna_have") }} <br />
+                {{ $t("to_be_secretly_in_love_with_each_other") }} <br />
+                {{ $t("and_leave_it_that") }}
+              </p>
+            </div>
+            <div
+              class="w-[700px] sm:max-w-[330px] text-3xl sm:text-sm text-primary-grey font-bold"
+            >
+              {{ $t("the_royal_tenenbaums") }},2001
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -111,32 +118,16 @@ import SuccessModal from "@/components/SuccessModal.vue";
 import LinkExpiredModal from "@/components/LinkExpiredModal.vue";
 import { usePaginationStore } from "@/store/pagination";
 import { useRoute, useRouter } from "vue-router";
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import { checkTokenIsValid } from "@/services/recovery";
 
 const route = useRoute();
 const router = useRouter();
 const paginationStore = usePaginationStore();
 
-const first = ref(null);
-const second = ref(null);
-const third = ref(null);
-
 const handleModalName = (name) => {
   document.documentElement.style.overflow = "hidden";
   paginationStore.updateModalName({ name });
-};
-
-const handleImageClick = (ref) => {
-  const { y } = ref.getBoundingClientRect();
-
-  if (y > 0) {
-    ref.scrollIntoView({ behavior: "smooth" });
-  } else {
-    document
-      .getElementById(Number(ref.id) + 1)
-      .scrollIntoView({ behavior: "smooth" });
-  }
 };
 
 onMounted(() => {
