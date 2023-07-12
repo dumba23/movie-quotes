d567fd6779030ef3ea97374d2e1fc2461405b24c
dumba23 <72528125+dumba23@users.noreply.github.com>
2023-07-12 18:48:19 +0400

    refactor: add translations and update design for quote components

--- src/components/QuoteNotificationCard.vue
+++ src/components/QuoteNotificationCard.vue
@@ -1,30 +1,41 @@
 <template>
   <div
-    class="flex justify-between border border-secondary-grey border-opacity-50 mb-2 p-2"
+    class="flex justify-between border border-secondary-grey border-opacity-50 mb-2 p-2 cursor-pointer"
   >
-    <div class="flex">
-      <img
-        :src="notification.sender.avatar"
-        alt="avatar"
-        class="h-10 w-10 rounded-full mr-4"
-        :class="notification.read === 0 && 'border-2 border-primary-success'"
-      />
-      <div class="flex flex-col">
-        <div>{{ notification.sender.username }}</div>
-        <div class="flex items-center">
-          <template v-if="notification.type === 'comment'">
-            <IconQuote class="h-4 w-4 mr-2" />
-          </template>
-          <template v-else>
-            <IconPinkHeart class="h-4 w-4 mr-2" />
-          </template>
-          <p>{{ notification.message }}</p>
+    <div class="flex flex-col">
+      <div class="flex">
+        <img
+          :src="notification.sender.avatar"
+          alt="avatar"
+          class="h-10 w-10 rounded-full mr-4"
+          :class="notification.read === 0 && 'border-2 border-primary-success'"
+        />
+        <div class="flex flex-col">
+          <div>{{ notification.sender.username }}</div>
+          <div class="flex items-center">
+            <template v-if="notification.type === 'comment'">
+              <IconQuote class="h-4 w-4 mr-2" />
+            </template>
+            <template v-else>
+              <IconPinkHeart class="h-4 w-4 mr-2" />
+            </template>
+            <p>{{ notification.message }}</p>
+          </div>
         </div>
       </div>
+      <div class="hidden sm:flex space-x-6 ml-1 mt-1 flex-row">
+        <div v-if="notification.read === 0" class="text-primary-success">
+          {{ $t("new") }}
+        </div>
+        <div v-else class="w-8"></div>
+        <div>{{ dayjs(String(notification.created_at)).fromNow() }}</div>
+      </div>
     </div>
-    <div class="flex flex-col items-end">
+    <div class="sm:hidden flex flex-col items-end">
       <div>{{ dayjs(String(notification.created_at)).fromNow() }}</div>
-      <div v-if="notification.read === 0" class="text-primary-success">New</div>
+      <div v-if="notification.read === 0" class="text-primary-success">
+        {{ $t("new") }}
+      </div>
     </div>
   </div>
 </template>
@@ -35,11 +46,12 @@ import IconQuote from "@/components/icons/IconQuote.vue";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/en";
-
-dayjs.extend(relativeTime);
-dayjs.locale("en");
+import i18n from "@/plugins/i18";
 
 defineProps({
   notification: { required: true, type: Object, default: () => {} },
 });
+
+dayjs.extend(relativeTime);
+dayjs.locale(i18n.global.locale.value);
 </script>
